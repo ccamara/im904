ab71095187c2c61c6d1c64129d6cf64d4d80e6f2
Carlos <carlos.camara@warwick.ac.uk>
2023-01-23 15:17:13 +0000

    Remove redundant syntax.

--- week2_rtweet.R
+++ week2_rtweet.R
@@ -14,4 +14,4 @@ tweets <- search_tweets(query_term, n=number_tweets, include_rts = FALSE)
 
 #Create the CSV name with the source, query and time and write it to local folder
 csvFileName = paste0("twitter", "_", query_term, "_", Sys.Date(), '.csv')
-rtweet::write_as_csv(tweets, paste0("data/week2/",csvFileName))
+write_as_csv(tweets, paste0("data/week2/",csvFileName))
